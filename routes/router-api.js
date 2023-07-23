032727d9d04201c0aa42f42570798e0b41c070bc
Marialdinha <marialda@bellsouth.net>
2023-07-23 12:40:26 -0500

    Updated delete router to make it work

--- routes/router-api.js
+++ routes/router-api.js
@@ -11,8 +11,11 @@ routerApi.get('/api/notes', (req, res) => {
         let notes = JSON.parse(data);
         res.json(notes);
     })
-    // let notes = JSON.parse(readData());
-    // res.json(notes);
+    // let notesData = readData();
+    // console.log("Inside get route")
+    // console.log (Promise.resolve(notesData))
+    // // let notes = JSON.parse(readData());
+    // // res.json(notes);
   });
 
 routerApi.post('/api/notes', (req, res) => {
@@ -32,13 +35,15 @@ routerApi.post('/api/notes', (req, res) => {
     })
 });
 
-routerApi.delete('/api/notes', (req, res) => {
+routerApi.delete('/api/notes/:id', (req, res) => {
   fs.readFile(dbFile,'utf8', function(err, data) {
     if (err) {console.log(err);}
     let notes = JSON.parse(data);
-    const newNotes = dataJSON.filter((notes) => { 
-      return notes.id !== req.params.id;
+    const newNotes = notes.filter((note) => { 
+      return note.id !== req.params.id;
     });
+
+    console.log(newNotes);
     fs.writeFileSync(dbFile, JSON.stringify(newNotes), (err) => {
       if (err) {console.log(err)}
       console.log("Notes deleted")
@@ -47,13 +52,11 @@ routerApi.delete('/api/notes', (req, res) => {
 
  });
 
-const readData = () =>{
-  fs.readFile(dbFile, 'utf8',function(err, data) {
-    if (err) {console.log(err);}
-
-    console.log("Reading data")
-    return data;
-})
+const readData = async () =>{
+  let notes = await fs.readFileSync(dbFile, 'utf8') 
+  console.log("Inside readData function")
+console.log(notes);
+return notes;
 }
 
 module.exports = routerApi;
